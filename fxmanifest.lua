1834919f504d37d85a1ff20d98dd4f99f7258340
EinSchnitzelchen <76046095+EinSchnitzelchen@users.noreply.github.com>
2022-07-12 16:32:25 +0200

    Adding fxmanifest
    Cant say more haha ^^

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'EinSchnitzelchen'
+description 'Simple Movement Script for ESX'
+version '1.0.0'
+
+dependency "es_extended"
+
+client_scripts {
+
+    'client.lua',
+
+}

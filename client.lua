3ad2f2d1d11b829239fb35c30927ec4289f57494
EinSchnitzelchen <76046095+EinSchnitzelchen@users.noreply.github.com>
2022-07-13 03:58:10 +0200

    Adding Crouch
    Added Crouch

--- client.lua
+++ client.lua
@@ -142,3 +142,36 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+
+local crouched = false
+
+Citizen.CreateThread(function()
+    while true do 
+        Citizen.Wait(1)
+
+        local plyPed = GetPlayerPed(-1)
+
+        if (DoesEntityExist(plyPed) and not IsEntityDead(plyPed)) then 
+            DisableControlAction(0, 36, true) -- INPUT_DUCK  
+
+            if (not IsPauseMenuActive()) then 
+                if (IsDisabledControlJustPressed(0, 36)) then 
+                    RequestAnimSet("move_ped_crouched")
+
+                    while (not HasAnimSetLoaded("move_ped_crouched")) do 
+                        Citizen.Wait(100)
+                    end 
+
+                    if (crouched == true) then 
+                        ResetPedMovementClipset(plyPed, 0)
+                        crouched = false 
+                    elseif (crouched == false) then
+                        SetPedMovementClipset(plyPed, "move_ped_crouched", 0.25)
+                        crouched = true 
+                    end 
+                end
+            end 
+        end 
+    end
+end)
